91dd2ba3a3aaa87f590c0003ac56306c09c78e8e
aba111f <karata93@mail.ru>
2025-02-16 00:43:27 +0500

    Practice 03

---
+++ Practice3/scripts/makeDirs.sh
@@ -0,0 +1,5 @@
+BASE_PATH="${1:-/home/aba/SoOS/Practice3}";
+
+mkdir -p "$BASE_PATH/public" && touch "$BASE_PATH/public/allData.txt";
+mkdir -p "$BASE_PATH/soos" && touch "$BASE_PATH/soos/topSecret.txt";
+
---
+++ Practice3/scripts/ugManage.sh
@@ -0,0 +1,7 @@
+sudo groupadd kbtusoos
+sudo useradd student1
+sudo useradd student2
+
+sudo usermod -aG kbtusoos student1
+sudo usermod -aG kbtusoos student2
+
